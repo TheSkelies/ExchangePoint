e4bb4fea63519334d621e9c45f61905d94194e0f
TheSkelies <The_Skelies@mail.ru>
2026-05-18 22:22:35 +0300

    adding main files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/layout/AppLayout.jsx";
+import RatesPage from "./pages/RatesPage.jsx";
+import HistoryPage from "./pages/HistoryPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import ExchangePage from "./pages/ExchangePage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProtectedRoute from "./components/routing/ProtectedRoute.jsx";
+import EditRates from "./pages/EditRates.jsx";
+
+export default function App() {
+    return (
+        <Routes>
+            <Route element={<AppLayout />}>
+                <Route index element={<Navigate to="/rates" replace />} />
+                <Route path="/rates" element={<RatesPage />} />
+                <Route path="/exchange/:rateId" element={<ExchangePage />} />
+                <Route
+                    path="/history"
+                    element={
+                    <ProtectedRoute allowedRoles={["seller"]}>
+                        <HistoryPage />
+                    </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <ProfilePage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/rates/edit"
+                    element={
+                        <ProtectedRoute allowedRoles={["seller"]}>
+                            <EditRates />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="*" element={<Navigate to="/rates" replace />} />
+            </Route>
+        </Routes>
+    );
+}
